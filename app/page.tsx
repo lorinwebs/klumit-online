8d8f7cce9ea971f80c184808db265b03879532da
Lorin Totah Hayat <youtublorin@gmail.com>
2026-01-25 23:47:14 +0200

    lorin

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import ValueProps from '@/components/ValueProps';
 import FeaturedProducts from '@/components/FeaturedProducts';
-import CustomerReviews from '@/components/CustomerReviews';
+// import CustomerReviews from '@/components/CustomerReviews';
 import InstagramFeed from '@/components/InstagramFeed';
 import type { Metadata } from 'next';
 
@@ -23,7 +23,7 @@ export default function Home() {
         <Hero />
         <ValueProps />
         <FeaturedProducts />
-        <CustomerReviews />
+        {/* <CustomerReviews /> */}
         <InstagramFeed />
       </main>
       <Footer />
