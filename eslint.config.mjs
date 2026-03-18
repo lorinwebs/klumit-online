e4a4bee98863feb540652e02b5a4cd752ba29f5a
totahLorin <lorintotah@gmail.com>
2026-03-18 10:55:28 +0200

    lorin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,10 @@
-import js from '@eslint/js';
-import nextPlugin from '@next/eslint-plugin-next';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import nextConfig from 'eslint-config-next';
 
 export default [
-  js.configs.recommended,
+  ...nextConfig,
   {
-    plugins: {
-      '@next/next': nextPlugin,
-      '@typescript-eslint': tsPlugin,
-      'react-hooks': reactHooksPlugin,
-    },
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
-      },
-    },
     rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      ...tsPlugin.configs.recommended.rules,
-      ...reactHooksPlugin.configs.recommended.rules,
+      '@next/next/no-img-element': 'off',
     },
   },
   {
